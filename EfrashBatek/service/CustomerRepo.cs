88fa22eec402288d38c229a1d47a2872862b28d1
Lenovo <Lenovo@DESKTOP-OGJ98U8>
2023-05-24 00:37:08 +0300

    alyaa

---
+++ EfrashBatek/service/CustomerRepo.cs
@@ -0,0 +1,41 @@
+﻿using EfrashBatek.Models;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using System.Collections.Generic;
+
+namespace EfrashBatek.service
+{
+    public class CustomerRepo
+    {
+
+        Context context;
+        public CustomerRepo(Context context)
+        {
+
+            this.context = context;
+
+        }
+        public void Create(Order Order, int CustomerID)
+        {
+            .
+
+
+             
+
+        }
+        public void CancelOrder (int OrderID) { 
+        
+        
+        }
+        // all orders cards 
+        public List<Order> ViewOrders () {
+
+            var Orders = new List<Order> ();  
+
+            return Orders  ;   
+        
+        
+        }
+    
+
+    }
+}
