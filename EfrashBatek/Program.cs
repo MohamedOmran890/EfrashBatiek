5f9f1cd2237742bc5a19392418b3476fba8c1088
Mohamed_Omran <mohomranyass@gmail.com>
2023-05-25 12:40:10 +0200

    finish

--- EfrashBatek/Program.cs
+++ EfrashBatek/Program.cs
@@ -36,12 +36,12 @@ namespace EfrashBatek
             context.Products.Add(new Product { ID = 2, ProductName = (ProductName)2, Description = "Wayfair is very good " });
             context.Products.Add(new Product { ID = 3, ProductName = (ProductName)3, Description = "Room is Very good " });
         }
-         var item = context.Items.FirstOrDefault(x => x.ID == 1);
-           if (item != null)
+            var brand = context.Brands.FirstOrDefault(x => x.ID == 1);
+            if (brand == null)
             {
-                context.Items.Add(new Item { ID = 1, Name = "serar", Brand_ID = 1, Code = "6548", Price = 5000, ShopID = 2, ProductID = 2, Image = "0.jpg" });
-                context.Items.Add(new Item { ID = 2, Name ="chair", Brand_ID =2,Code="87865",Price=5000,ShopID=2,ProductID=2,Image="1.jpg" });
-                context.Items.Add(new Item { ID = 3, Name = "trtr", Brand_ID =2, Code = "7969", Price = 5000, ShopID = 2, ProductID = 2, Image = "2.jpg" });
+                context.Brands.Add(new Brand { ID = 1, Name = "Addidas", Category=(Category)1});
+                context.Brands.Add(new Brand { ID = 2, Name = "mesh aerf", Category = (Category)2 });
+                context.Brands.Add(new Brand { ID = 3, Name = "tabaan ya capten", Category = (Category)3 });
             }
             var shop = context.Shops.FirstOrDefault(x => x.ID == 1);
             if(shop==null)
@@ -51,12 +51,12 @@ namespace EfrashBatek
                 context.Shops.Add(new Shop { ID = 3, Name = "manarh", ShopNumber=54875,ShopAddress="Assiut"});
             }
 
-            var brand = context.Brands.FirstOrDefault(x => x.ID == 1);
-            if (shop == null)
+         var item = context.Items.FirstOrDefault(x => x.ID == 1);
+           if (item != null)
             {
-                context.Brands.Add(new Brand { ID = 1, Name = "Addidas", Category=(Category)1});
-                context.Brands.Add(new Brand { ID = 2, Name = "mesh aerf", Category = (Category)2 });
-                context.Brands.Add(new Brand { ID = 3, Name = "tabaan ya capten", Category = (Category)3 });
+                context.Items.Add(new Item { ID = 1, Name = "serar", Brand_ID = 1, Code = "6548", Price = 5000, ShopID = 2, ProductID = 2, Image = "0.jpg",Description="yotuu",QuantityInStore=3 });
+                context.Items.Add(new Item { ID = 2, Name ="chair", Brand_ID =2,Code="87865",Price=5000,ShopID=2,ProductID=2,Image="1.jpg", Description = "yotuu", QuantityInStore = 3 });
+                context.Items.Add(new Item { ID = 3, Name = "trtr", Brand_ID =2, Code = "7969", Price = 5000, ShopID = 2, ProductID = 2, Image = "2.jpg", Description = "yotuu", QuantityInStore = 3 });
             }
 
             context.SaveChanges();
