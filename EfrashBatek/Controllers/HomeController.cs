d652b98ab5f1db047b660b380ee5f851701e0781
Lenovo <Lenovo@DESKTOP-OGJ98U8>
2023-05-27 00:48:23 +0300

    done

--- EfrashBatek/Controllers/HomeController.cs
+++ EfrashBatek/Controllers/HomeController.cs
@@ -22,10 +22,40 @@ namespace EfrashBatek.Controllers
 
         }
 
-        public IActionResult Index()
+        public IActionResult TrendingProducts()
         {
-            var ans = _Product.GetAll();
-            return View(ans);
+            //var ans = _Product.GetAll();
+            Item item = new Item();
+            item.Name = "Bedroom";
+           
+            
+            item.Image = "Brown right L shaped sofa.jpg";
+            item.Image2 = "LF-L000301.jpg";
+            item.Price = 78;
+            item.discount = "%15";
+            item.PriceAfterSale = 48;
+
+            Item item2 = new Item();
+            item2.Name = "Bedroom";
+            
+            item2.Image = "Brown right L shaped sofa.jpg";
+            item2.Image2 = "LF-L000301.jpg";
+            item2.Price = 78;
+            item2.discount = "%15";
+            item2.PriceAfterSale = 48;
+
+            List<Item> list = new List<Item>(); 
+            list.Add(item);
+            list.Add(item2);    
+
+
+
+
+            Product product = new Product() { Items = list };
+            Product product2 = new Product() { Items = list };
+            List<Product> products = new List<Product>()
+            { product , product2 };
+            return View( products);
             /*************************/
         }
 
@@ -33,7 +63,12 @@ namespace EfrashBatek.Controllers
         {
             return View();
         }
-        public IActionResult About()
+        // contact us
+        public IActionResult ContactUs()
+        {
+            return View();
+        }
+        public IActionResult AboutUs()
         {
             return View();
         }
