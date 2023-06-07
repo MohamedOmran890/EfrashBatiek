22ae3acc900f557c34ce0b71e916a2230dc2ffcc
Lenovo <Lenovo@DESKTOP-OGJ98U8>
2023-06-07 22:53:52 +0300

    done

---
+++ EfrashBatek/Models/RoleEdit.cs
@@ -0,0 +1,16 @@
+﻿
+using Microsoft.AspNetCore.Identity;
+using System.Collections.Generic;
+
+namespace EfrashBatek.Models
+{
+  
+    
+        public class RoleEdit
+        {
+            public IdentityRole Role { get; set; }
+            public IEnumerable<User> Members { get; set; }
+            public IEnumerable<User> NonMembers { get; set; }
+        }
+    
+}
