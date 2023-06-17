24b1eba840f616ba91c412cdb6d54828313aff88
Aya Abdelkhalk <aya.yahoo3070@gmail.com>
2023-06-17 10:51:43 +0300

    Aya2

--- EfrashBatek/Controllers/ItemController.cs
+++ EfrashBatek/Controllers/ItemController.cs
@@ -4,7 +4,7 @@ namespace EfrashBatek.Controllers
 {
     public class ItemController : Controller
     {
-        public IActionResult Index()
+        public IActionResult ProductaDetails()
         {
             return View();
         }
